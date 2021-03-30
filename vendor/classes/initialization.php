f1d2a033c07ef32630753268e47681f4ae8efb6a
Dremax <ramazan.batyaev@yandex.ru>
2021-03-30 21:35:44 +0300

    добавление prolog_before

--- vendor/classes/initialization.php
+++ vendor/classes/initialization.php
@@ -65,7 +65,7 @@ class initialization
                 "NAME"           => $arrDump["lastname"].' '.$arrDump["firstname"].' '.$arrDump["surname"],
                 "ACTIVE"         => "Y",            // активен
             ];
-            Iblock\ElementTable::add($arInfo);
+//            Iblock\ElementTable::add($arInfo);
         }
         else
         {
