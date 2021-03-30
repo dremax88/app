c986b3bf30ae2cc8ea51c515387fb2374e5e4a8c
Dremax <ramazan.batyaev@yandex.ru>
2021-03-30 21:47:21 +0300

    добавление prolog_before

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $a=$a::parseArr($_REQUEST);
 var_dump($a);
 $PROP = $_REQUEST;
 $arInfo = [
-
+    "MODIFIED_BY"    => 1, // элемент изменен текущим пользователем
     "IBLOCK_SECTION_ID" => false,          // элемент лежит в корне раздела
     "IBLOCK_ID"      => 61,
     "PROPERTY_VALUES"=> $PROP,
