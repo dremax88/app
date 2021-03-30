53f6888827c8d5935f0933f2c83fcdf3243e4ad9
Dremax <ramazan.batyaev@yandex.ru>
2021-03-30 15:42:44 +0300

    1 commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'aa7488906db5e3276a0190a1df90f3ca1acb8d82',
+    'name' => 'vendor_name/app',
+  ),
+  'versions' => 
+  array (
+    'vendor_name/app' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aa7488906db5e3276a0190a1df90f3ca1acb8d82',
+    ),
+  ),
+);
