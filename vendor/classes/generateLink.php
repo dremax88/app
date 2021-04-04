77cf228246a065e385f720d0a28ecf83f0e9230a
Dremax <ramazan.batyaev@yandex.ru>
2021-04-04 15:05:21 +0300

    Стабильная версия с применением SOLID v1

--- vendor/classes/generateLink.php
+++ vendor/classes/generateLink.php
@@ -6,5 +6,46 @@ namespace classes;
 
 class generateLink
 {
+    protected $idUser, $ufType;
 
+    public function __construct($idUser, $ufType)
+    {
+        $this->idUser=$idUser;
+        $this->ufType=$ufType;
+    }
+
+    public function getGenerateUfLink()
+    {
+        $arUser=self::UserArr($this->idUser,$this->ufType);
+        $hech = uniqid();
+        switch ($this->ufType)
+        {
+            case 'UF_LINK':
+
+                $link = "https://rolf.si-24.ru/questionnaires/negative/?avto=no&hech=".$hech."&admid=".$idUser."&type_of_questionnaire=Negative";
+                $arLink['link']=$link;
+                $arUser[$this->ufType][]=$link;
+                $arLink[$this->ufType]=$arUser[$this->ufType];
+                break;
+
+            case 'UF_SOTR_LINK':
+
+                $link = "https://rolf.si-24.ru/questionnaires/negative/?avto=no&hech=".$hech."&admid=".$idUser."&type_of_questionnaire=HR";
+                $arLink['link']=$link;
+                $arUser[$this->ufType][]=$link;
+                $arLink[$this->ufType]=$arUser[$this->ufType];
+                break;
+
+            case 'UF_KADRSH':
+                $link = "https://rolf.si-24.ru/questionnaires/negative/?avto=no&hech=".$hech."&admid=".$idUser."&type_of_questionnaire=Extended";
+                $arLink['linck']=$link;
+                $arUser[$this->ufType][]=$link;
+                $arLink[$this->ufType]=$arUser[$this->ufType];
+                break;
+            default:
+                $arLink=false;
+        }
+        return $arLink;
+
+    }
 }
--- vendor/classes/initialization.php
+++ vendor/classes/initialization.php
@@ -45,37 +45,8 @@ class initialization
 
     public static function generateUfLink($idUser, $ufType)
     {
-        $arUser=self::UserArr($idUser,$ufType);
-        $hech = uniqid();
-        switch ($ufType)
-        {
-            case 'UF_LINK':
-
-                $link = "https://rolf.si-24.ru/questionnaires/negative/?avto=no&hech=".$hech."&admid=".$idUser."&type_of_questionnaire=Negative";
-                $arLink['link']=$link;
-                $arUser[$ufType][]=$link;
-                $arLink[$ufType]=$arUser[$ufType];
-                break;
-
-            case 'UF_SOTR_LINK':
-
-                $link = "https://rolf.si-24.ru/questionnaires/negative/?avto=no&hech=".$hech."&admid=".$idUser."&type_of_questionnaire=HR";
-                $arLink['link']=$link;
-                $arUser[$ufType][]=$link;
-                $arLink[$ufType]=$arUser[$ufType];
-                break;
-
-            case 'UF_KADRSH':
-                $link = "https://rolf.si-24.ru/questionnaires/negative/?avto=no&hech=".$hech."&admid=".$idUser."&type_of_questionnaire=Extended";
-                $arLink['linck']=$link;
-                $arUser[$ufType][]=$link;
-                $arLink[$ufType]=$arUser[$ufType];
-                break;
-            default:
-                $arLink=false;
-        }
-        return $arLink;
-
+        $arUser=new generateLink($idUser,$ufType);
+        return $arUser->getGenerateUfLink();
     }
 
 
