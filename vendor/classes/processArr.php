9f7c74669aec26664d6f2ba5f7d56295861f38f0
Dremax <ramazan.batyaev@yandex.ru>
2021-04-06 18:12:13 +0300

    Стабильная версия с применением SOLID v1 email

--- vendor/classes/processArr.php
+++ vendor/classes/processArr.php
@@ -29,7 +29,7 @@ class processArr
             $count=0;
             foreach ($this->arrDump as $key=>$value)
             {
-                echo $key.'<br>';
+                echo strpos('work_company', $key) .'<br>';
                 if(strpos('work_company', $key) !== false)
                 {
                     $count++;
