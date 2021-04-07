0b95469f3b1e34b918ca9aff4104b5d7f08a2500
Dremax <ramazan.batyaev@yandex.ru>
2021-04-07 10:02:25 +0300

    Стабильная версия с применением SOLID v1 email

--- vendor/classes/processArr.php
+++ vendor/classes/processArr.php
@@ -48,6 +48,7 @@ class processArr
             $this->arrDump['work_company']=$arrWork_company;
             $this->arrDump['work_position']=$arrWork_position;
             if($key==='data_b'){
+                echo $this->arrDump['data_b'];
                 $date=explode('.',$this->arrDump[$key]);
                 $date=$date[2].'-'.$date[1].'-'.$date[0];
                 $this->arrDump['data_b']=$date;
